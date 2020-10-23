1a8b0c1998ef9e2c42e21b8dce71ac473abc2e8b
BoKyeong-Kim <bokyeong3659@gmail.com>
2020-10-23 12:13:59 +0900

    chat application - useEffect chat.js

---
+++ Chat/client/src/components/Chat/Chat.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+const Chat = ( {location} ) => {
+    useEffect(() => {
+        const data = queryString.parse(location.search);
+
+        console.log(location.search);
+        console.log(data);
+    })
+
+    return(
+        <h1>Chat</h1>
+    )
+}
+
+export default Chat;
--- Chat/server/index.js
+++ Chat/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketio = require('socket.io'); // http 요청이 느리기때문에 소켓을 사용하는게 좋음
 const http = require('http');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
 
@@ -11,6 +11,14 @@ const app = express();
 // node.js 애플리케이션 사용, 서버 초기화하여 설정할수있도록 서버를 만들고 express에사 초기화한 해당 앱을 전달해야한다.
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('We have a new connection!!!');
+
+    socket.on('disconnect', () =>{
+        console.log('User had left!!!');
+    }) 
+}); //기본 클라이언트 측을 생성하여 실제로 실시간 수신, 연결 및 해제
 
 app.use(router);
 
