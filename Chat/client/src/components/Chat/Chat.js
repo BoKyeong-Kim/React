4846a5fbeaa9baca60fb59f329bfab2d7d195afc
BoKyeong-Kim <bokyeong3659@gmail.com>
2020-10-31 19:04:19 +0900

    add info bar - information room name

--- Chat/client/src/components/Chat/Chat.js
+++ Chat/client/src/components/Chat/Chat.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import './Chat.css';
+
+import InfoBar from '../InfoBar/InfoBar';
 let socket;
+
 
 const Chat = ( {location} ) => {
     const [name, setName] = useState(''); 
@@ -49,11 +53,12 @@ const Chat = ( {location} ) => {
     return(
         <div className="outerContainer">
             <div className="container">
-                <input
+                <InfoBar room={ room }/>
+                {/*<input
                 value={message}
                 onChange={(event) => setMessage(event.target.value)}
                 onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
-                />
+                />*/}
             </div>
         </div>
     )
