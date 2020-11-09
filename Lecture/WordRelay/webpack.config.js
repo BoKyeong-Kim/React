3a9933460b26f8652be72eec7588f34ea5d8ead0
BoKyeong-Kim <bokyeong3659@gmail.com>
2020-11-09 15:56:15 +0900

    finish webpack setting

--- Lecture/WordRelay/webpack.config.js
+++ Lecture/WordRelay/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     name: 'wordrelay-setting',
@@ -9,20 +10,29 @@ module.exports = {
     },
 
     entry: {
-        app: ['./client'], // client.jsx가 WordRelay를 불러와서 client만 적어준다.
+        app: './client', // client.jsx가 WordRelay를 불러와서 client만 적어준다.
     }, // 입력
 
     module: {
         rules: [{
-            test: /.\jsx?/, //js와 jsx파일에 rule 적용
+            test: /.\jsx?$/, //js와 jsx파일에 rule 적용
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties']
+                presets: [
+                    ['@babel/preset-env', { 
+                targets: {
+                    browsers : ['> 5% in KR', 'last 2 chrome version'], //browserlist
+                    },
+                    debug : true
+                }], '@babel/preset-react'
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties']
             },
         }],
     },
-
+    plugins: [ //추가적으로 하고싶은 작업
+        new webpack.LoaderOptionsPlugin({debug:true})
+    ],
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'app.js'
