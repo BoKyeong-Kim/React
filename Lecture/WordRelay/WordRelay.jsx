1b0dbf2a2fa5ed8ddcdbdb5bc9a326cf54844dc3
BoKyeong-Kim <bokyeong3659@gmail.com>
2020-11-11 17:36:40 +0900

    create wordreply game - hooks

--- Lecture/WordRelay/WordRelay.jsx
+++ Lecture/WordRelay/WordRelay.jsx
@@ -1,14 +1,42 @@
 const React = require('react');
-const { Component } = React;
+const { useState, useRef } = React;
 
-class WordRelay extends Component {
-    state = {
-        text: 'Hello, webpack'
-    };
+const WordRelay = () => {
+    const [word, setWord] = useState('보카');
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const inputRef = useRef(null);
 
-    render() {
-        return <h1>{this.state.text}</h1>
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        if(word[word.length - 1] === value[0]) {
+             //함수라는 것 유의 !! 
+            setResult('딩동댕');
+            setWord(value);
+            setValue('');
+            inputRef.current.focus();
+        } else {
+            setResult('땡');
+            setValue('');
+            inputRef.current.focus();
+        }
     }
+
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
+    }
+
+
+    return (
+        <>
+            <div>{word}</div>
+            <form onSubmit={onSubmitForm}>
+                <input ref={inputRef} onChange={onChangeInput} value={value}/>
+                <button>입력!</button>
+            </form>
+            <div>{result}</div>
+        </>
+    )
 }
 
 module.exports = WordRelay;
