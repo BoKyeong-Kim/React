14f0f44d6b049b691136edd3595b211c0d8aebe0
BoKyeong-Kim <bokyeong3659@gmail.com>
2020-11-28 11:37:51 +0900

    finish response check setting

--- Lecture/ResponseCheck/ResponseCheck.jsx
+++ Lecture/ResponseCheck/ResponseCheck.jsx
@@ -1,14 +1,38 @@
-import React, { Componenet } from 'react';
-import { throws } from 'assert';
+import React, { Component } from 'react';
 
-class ResponseCheck extends Componenet {
+class ResponseCheck extends Component {
     state = {
         state : 'waiting', //배경에 대한 상태
-        message : '클릭해서 시작하세요.',
+        message : '클릭해서 시작하세요',
         result : [],
     };
 
     onClickScreen = () => {
+        const { state, message, result } = this.state //구조분해
+        if (state === 'waiting') {
+            this.setState({
+               state : 'ready',
+               message : '초록색이 되면 클릭하세요.'
+            });
+            setTimeout(() => {
+                this.setState({
+                    state : 'now',
+                    message : '지금 클릭'                
+                })
+            }, Math.floor(Math.random() * 1000) + 2000); //2초 ~ 3초 랜덤
+        } else if (state === 'ready') { //성급하게 클릭
+            this.setState({
+                state : 'waiting',
+                message : '너무 성급하시군요! 초록색이 된 후에 클릭하세요.'
+            })
+        } else if (state === 'now') { // 반응속도 체크
+            this.setState({
+                state : 'waiting',
+                result : [],
+                message : '클릭해서 시작하세요.'
+            })
+
+        }
 
     }
 
---
+++ Lecture/ResponseCheck/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+    name: 'responsecheck-setting',
+    mode: 'development', //실서비스 : production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app: './client',
+    }, // 입력
+
+    module: {
+        rules: [{
+            test: /.\jsx?$/, //js와 jsx파일에 rule 적용
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', { 
+                targets: {
+                    browsers : ['> 5% in KR', 'last 2 chrome version'], //browserlist
+                    },
+                    debug : true
+                }], '@babel/preset-react'
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel'
+                ]
+            },
+        }],
+    },
+    plugins: [ //추가적으로 하고싶은 작업
+        new RefreshWebpackPlugin
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js',
+        publicPath : '/dist/',
+    }, // 출력
+    devServer: { //소스코드의 변경점을 확인해줌 
+        publicPath : '/dist/',
+        hot : true,
+    }
+}
