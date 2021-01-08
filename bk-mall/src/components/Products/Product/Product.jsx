60948f5a9319e2f3b8b962e06d9e0325162f2f76
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-01-08 15:51:56 +0900

    connect commerce.js api - add product contents

--- bk-mall/src/components/Products/Product/Product.jsx
+++ bk-mall/src/components/Products/Product/Product.jsx
@@ -6,6 +6,8 @@ import useStyles from './styles'
 const Product = ({product}) => {
     const classes = useStyles();
 
+    console.log(product);
+    
     return(
         <Card className={classes.root}>
             <CardMedia className={classes.media} title={product.name} />
@@ -15,10 +17,10 @@ const Product = ({product}) => {
                         {product.name}
                     </Typography>
                     <Typography variant="h5" color="textSecondary" gutterBottom>
-                        {product.price}
+                        {product.price.formatted_with_symbol}
                     </Typography>
                 </div>
-                <Typography variant="body2" color="textSecondary"/>
+                <Typography variant="body2" dangerouslySetInnerHTML={{__html: product.description}} color="textSecondary" />
             </CardContent>
             <CardActions disableSpacing className={classes.cardActions}>
                 <IconButton aria-label="Add to Cart">
@@ -27,6 +29,7 @@ const Product = ({product}) => {
             </CardActions>
         </Card>
     )
+
 }
 
 export default Product;
--- bk-mall/src/components/Products/Products.jsx
+++ bk-mall/src/components/Products/Products.jsx
@@ -4,24 +4,15 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product'
 import useStyles from './styles';
 
-const product_test = [
-    { id : 1, name : 'iphone', price : '$100', description : 'apple i phone'},
-    { id : 2, name : 'macbook', price : '$350', description : 'apple mac book pro'},
-    { id : 3, name : 'apple watch', price : '$90', description : 'apple watch 6'},
-    { id : 4, name : 'air pot', price : '$55', description : 'apple air pot pro'}
-]
-
-const Products = ({products}) => {
+const Products = ({ products }) => {
     const classes = useStyles();
-
-    console.log(product_test)
 
     return (
         <main className={classes.content}>
             <div className = {classes.toolbar} />
             <Grid container justify="center" spacing={4}>
-                {product_test.map((product) => (
-                    <Grid item key={product_test.id} xs={12} sm={6} md={4} lg={3}>
+                {products.map((product) => (
+                    <Grid item key={products.id} xs={12} sm={6} md={4} lg={3}>
                        <Product product={product}/>
                     </Grid>
                 ))}
