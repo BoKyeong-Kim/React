348fd7f854a0c2384c0bd06ab71cf0b0951ebec3
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-01-30 16:05:19 +0900

    add detailview component in  product

--- bk-mall/src/components/Products/Product/Product.jsx
+++ bk-mall/src/components/Products/Product/Product.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
-import useStyles from './styles'
+import useStyles from './styles';
 
-import BaseProduct from './BaseProduct'
+import BaseProduct from './BaseProduct';
+import DetailView from './DetailView';
 
 const Product = ({ product, onAddToCart }) => {
     const classes = useStyles();
 
     return(
-        <Card className={classes.root}>
+        <>
+          <DetailView />
+            <Card className={classes.root}>
             <CardMedia className={classes.media} title={product.name} image={product.media.source}/>
             <CardContent>
             <BaseProduct product={product}/>
@@ -21,6 +24,7 @@ const Product = ({ product, onAddToCart }) => {
             </CardActions>
             </CardContent>
         </Card>
+        </>
     )
 
 }
