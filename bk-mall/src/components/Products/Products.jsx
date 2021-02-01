22d10180369e452b60dd45e6cf0972a7f5fcd32a
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-02-01 17:21:15 +0900

    create modal function - plan to delete

--- bk-mall/src/components/Products/Products.jsx
+++ bk-mall/src/components/Products/Products.jsx
@@ -4,16 +4,17 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product'
 import useStyles from './styles';
 
-const Products = ({ products, onAddToCart }) => {
+const Products = ({ products, onAddToCart, fetchProduct, detail }) => {
     const classes = useStyles();
 
     return (
         <main className={classes.content}>
             <div className = {classes.toolbar} />
+            {/* 여기에 modal component 추가 - detail props 넘김 */}
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={products.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} onAddToCart={onAddToCart}/>
+                        <Product product={product} onAddToCart={onAddToCart} fetchProduct={fetchProduct} detail={detail}/>
                     </Grid>
                 ))}
             </Grid>
