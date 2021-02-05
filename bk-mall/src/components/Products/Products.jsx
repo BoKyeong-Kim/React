23059292f3dc77dba21b3d29310d964adb08fcb2
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-02-05 20:57:01 +0900

    add modal component to map function as loop statement

--- bk-mall/src/components/Products/Products.jsx
+++ bk-mall/src/components/Products/Products.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
-import Product from './Product/Product'
+import Product from './Product/Product';
+import Modal from './Modal/Modal';
 import useStyles from './styles';
 
 const Products = ({ products, onAddToCart, fetchProduct, detail }) => {
     const classes = useStyles();
-
+    
     return (
         <main className={classes.content}>
             <div className = {classes.toolbar} />
-            {/* 여기에 modal component 추가 - detail props 넘김 */}
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={products.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} onAddToCart={onAddToCart} fetchProduct={fetchProduct} detail={detail}/>
+                        <Product product={product} onAddToCart={onAddToCart}/>
+                        <Modal fetchProduct={fetchProduct} product={product}/>
                     </Grid>
                 ))}
             </Grid>
