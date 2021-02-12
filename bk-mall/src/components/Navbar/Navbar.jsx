271601d6d01f177691d37cefe02a5af7b53dcf88
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-02-12 15:03:42 +0900

    modify index.js file and Navbar component

--- bk-mall/src/components/Navbar/Navbar.jsx
+++ bk-mall/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, IconButton, Badge } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import { Link, useLocation } from 'react-router-dom';
+import  Darkmode from './Darkmode/Darkmode';
 
 import logo from '../../assets/rocketlogo.png'
 import useStyles from './styles';
@@ -9,15 +10,15 @@ import useStyles from './styles';
 const Navbar = ({ totalItems }) => { 
     const classes = useStyles();
     const location = useLocation();
-    
+
     return (
-        <>
-            <AppBar position="fixed" className={classes.appbar} >
+        <AppBar position="fixed" className={classes.appbar} >
                 <Toolbar>
                     <Typography className={classes.title} color="inherit">
                         <img className={classes.image} src={logo} alt="bkmall" height="25px" />
                         BK-mall
                     </Typography>
+                    <Darkmode />
                     <div className={classes.grow} />
                     {location.pathname === "/" && (
                         <div className={classes.button}>
@@ -29,7 +30,6 @@ const Navbar = ({ totalItems }) => {
                     </div>)}
                 </Toolbar>
             </AppBar>
-        </>
     )
 }
 
