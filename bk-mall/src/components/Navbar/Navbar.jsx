9e0ad0283a3a2a59afb6af6739ae010bf497f04e
김보경 <gimbogyeong@gimbogyeong-ui-MacBookPro.local>
2021-02-13 20:12:05 +0900

    add navbar component and dark mode component to navbar component

--- bk-mall/src/components/Navbar/Navbar.jsx
+++ bk-mall/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Badge } from '@material-ui/core';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, IconButton, Badge, CssBaseline, Switch  } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import { Link, useLocation } from 'react-router-dom';
-import  Darkmode from './Darkmode/Darkmode';
+import clsx from "clsx";
+import {orange, lightBlue, deepPurple, deepOrange} from "@material-ui/core/colors";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 import logo from '../../assets/rocketlogo.png'
 import useStyles from './styles';
 
 const Navbar = ({ totalItems }) => { 
+    const [darkState, setDarkState] = useState(false);
+    const palletType = darkState ? "dark" : "light";
+    const mainPrimaryColor = darkState ? orange[500] : lightBlue[500];
+    const mainSecondaryColor = darkState ? deepOrange[900] : deepPurple[500];
+    const darkTheme = createMuiTheme({
+    palette: {
+      type: palletType,
+      primary: {
+        main: mainPrimaryColor
+      },
+      secondary: {
+        main: mainSecondaryColor
+      }
+    }
+  });
     const classes = useStyles();
     const location = useLocation();
+    const handleThemeChange = () => {
+        setDarkState(!darkState);
+    };
 
     return (
-        <AppBar position="fixed" className={classes.appbar} >
-                <Toolbar>
-                    <Typography className={classes.title} color="inherit">
+        <ThemeProvider theme={darkTheme}>
+            <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="absolute"
+          className={clsx(classes.appBar, classes.appBarShift)}
+         >
+                <Toolbar className={classes.toolbar}>
+                    <Typography className={classes.title} component="h1"
+                        variant="h6"
+                        color="inherit"
+                        noWrap>
                         <img className={classes.image} src={logo} alt="bkmall" height="25px" />
                         BK-mall
                     </Typography>
-                    <Darkmode />
                     <div className={classes.grow} />
                     {location.pathname === "/" && (
                         <div className={classes.button}>
@@ -28,8 +55,12 @@ const Navbar = ({ totalItems }) => {
                             </Badge>
                         </IconButton>
                     </div>)}
+                    <Switch checked={darkState} onChange={handleThemeChange} className={classes.button}/>
+                    
                 </Toolbar>
             </AppBar>
+            </div>
+            </ThemeProvider>
     )
 }
 
