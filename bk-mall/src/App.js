efb08983647bae0911a00c72c019c3f962e5f47b
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-02-23 11:42:05 +0900

    add checkout path

--- bk-mall/src/App.js
+++ bk-mall/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
-import { Products, Navbar, Cart } from './components/';
+import { Products, Navbar, Cart, Checkout } from './components/';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState({});
+    const [order, setOrder] = useState({});
     
     const fetchProducts = async () => { 
         const { data } = await commerce.products.list();
@@ -41,6 +42,20 @@ const fetchCart = async () => {
         setCart(cart);
     }
 
+    const refreshCart = async () => {
+        const newCart = await commerce.cart.refresh();
+
+        setCart(newCart);
+    }
+
+    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+            const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+            setOrder(incomingOrder);
+            refreshCart();
+    }
+
+
     useEffect(() => {
         fetchProducts();
         fetchCart();
@@ -68,7 +83,11 @@ const fetchCart = async () => {
                         />
                     </Route>
                     <Route exact path="/checkout">
-
+                        <Checkout
+                            cart={cart}
+                            order={order}
+                            onCaptureCheckout={handleCaptureCheckout}
+                        />
                     </Route>
                 </Switch>
             </div>
