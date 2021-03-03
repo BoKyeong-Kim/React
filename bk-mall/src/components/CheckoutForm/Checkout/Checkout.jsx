7bf36d52d59f8a6496b861ed74f4af3389ebfb04
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-03-04 08:50:57 +0900

    add if statement in checkout

--- bk-mall/src/components/CheckoutForm/Checkout/Checkout.jsx
+++ bk-mall/src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Paper, StepLabel, Typography, Stepper, Step } from '@material-ui/core';
+import { Paper, StepLabel, Typography, Stepper, Step, CircularProgress, Divider, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { commerce } from '../../../lib/commerce';
 import useStyle from './styles';
 import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping address', 'Payment datails'];
 
@@ -15,11 +16,13 @@ const Checkout = ({ cart, order, onCaptureCheckout }) => {
 
     const classes = useStyle();
 
+
     useEffect(() => {
         const generateToken = async () => {
-            const token = await commerce.checkout.generateToken(cart.id, { type : 'cart'});
+                const token = await commerce.checkout.generateToken(cart.id, { type : 'cart'});
 
-            setCheckoutToken(token);
+                setCheckoutToken(token);
+
         }
         generateToken();
     }, [cart]);
@@ -31,23 +34,39 @@ const Checkout = ({ cart, order, onCaptureCheckout }) => {
         setShippingData(data);
 
         nextStep();
-    }
+    };
+
 
     let Confirmation = () => order.customer ? (
         <>
+            <div>
+                <Typography variant='h5'>Thank you for your purchase, {order.customer.firstname}, {order.customer.lastname} </Typography>
+                <Divider className={classes.divider}/>
+                <Typography variant='subtitle2'>Order ref : {order.customer_reference}</Typography>
+            </div>
+            <br/>
+            <Button component={Link} to="/" variant='outlined' type='button'>Back to Home</Button>
         </>
     ) : isFinished ? (
         <>
+            <div>
+                <Typography variant='h5'>Thank you for your purchase </Typography>
+                <Divider className={classes.divider}/>
+            </div>
+            <br/>
+            <Button component={Link} to="/" variant='outlined' type='button'>Back to Home</Button>
         </>
-    ) : (
-        <>
-        </>
+    ) :(
+            <div className={classes.spinner}>
+                <CircularProgress />
+            </div>
     );
 
 
     const Form = () => activeStep === 0
     ? <AddressForm checkoutToken={checkoutToken} next={next}/>
-    : 'payment'
+    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} onCaptureCheckout={onCaptureCheckout} />
+
 
     return (
         <>
